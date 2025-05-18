dcb2c7bdb0d515410e4fac687689a375f23a51e5
Ricky Liu <101468308+ScripterRickster@users.noreply.github.com>
2025-05-18 10:19:51 -0400

    Update and rename app.py to webapp.py

--- webapp.py
+++ webapp.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, render_template, redirect, url_for
 import subprocess
 
+#import Pushups
+#import Situps
+#import Tracker
+
 app = Flask(__name__)
 
 trackersRunning = {
@@ -15,23 +19,46 @@ statTracker = {
     "calories_burnt":0,
 }
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        act = request.form.get('action')
+
+        if act == 's_pushup':
+            trackPushups()
+            print("pushup")
+        elif act == 's_situp':
+            trackSitups()
+            print("situp")
+        elif act == 's_demo':
+            trackDemo()
+            print("demo")
+
+        return redirect(url_for('index'))
+    return render_template('index.html')
+
+@app.route('/stats')
+def stats():
+    return render_template('stats.html')
+
 def trackPushups():
     if not trackersRunning["pushups"]:
         trackersRunning["pushups"] = True
-        subprocess.run(['python3', 'Pushups.py'])
-    return "yes"
+        print(statTracker["pushups"])
+        #subprocess.run(['python3', 'Pushups.py'])
+
 
 def trackSitups():
     if not trackersRunning["situps"]:
         trackersRunning["situps"] = True
-        subprocess.run(['python3', 'Situps.py'])
-    return "yes"
+        #subprocess.run(['python3', 'Situps.py'])
+
 
 def trackDemo():
     if not trackersRunning["demo"]:
         trackersRunning["demo"] = True
-        subprocess.run(['python3', 'Tracker.py'])
-    return "yes"
+        #subprocess.run(['python3', 'Tracker.py'])
+
 
 def updateStats(stat,val):
     if statTracker[stat]:
@@ -45,32 +72,14 @@ def resetTracker(stat):
 
 def home():
     return render_template('index.html')
-@app.route('/stats')
-def stats():
-    return render_template('stats.html')
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        act = request.form.get('action')
-        app.logger.info(f"Received POST request with action: {act}")  # Added logging
-
-        if act == 's_pushup':
-            trackPushups()
-            print("pushup")
-        elif act == 's_situp':
-            trackSitups()
-            print("situp")
-        elif act == 's_demo':
-            trackDemo()
-            print("demo")
-
-        return redirect(url_for('index'))
-
-    return render_template('index.html')
 
 
 
-#if __name__ == '__main__':
-    #app.run(debug=True)
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
